0c2eefaf5cba61c2e90d622534bcbd19e9e38dad
Maxim Tincu <tincumax@mail.ru>
2016-05-20 12:14:44 +0300

    First commit

---
+++ Unit5.cpp
@@ -0,0 +1,15 @@
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Unit5.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma link "Word_XP_srvr"
+#pragma resource "*.dfm"
+TForm5 *Form5;
+//---------------------------------------------------------------------------
+__fastcall TForm5::TForm5(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
